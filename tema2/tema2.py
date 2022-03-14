d0b2dd7ae8dda32be486d3fd81fac74bff9ddb7f
Alexandra <alexandramosoiu19@gmail.com>
2022-03-14 23:09:20 +0200

    prima incercare

--- tema2/tema2.py
+++ tema2/tema2.py
@@ -6,25 +6,77 @@ def substitution_method(A, b, n):
         raise ValueError("The matrix is singular")
     else:
         x = list(0 for j in range(0, n))
-        x[n-1] = b[n-1]/A[n-1][n-1]
-        for i in range(n-2, -1, -1):
+        x[n - 1] = b[n - 1] / A[n - 1][n - 1]
+        for i in range(n - 2, -1, -1):
             suma = 0
-            for j in range(i+1, n):
-                suma = suma + A[i][j]*x[j]
-            v = b[i]-suma
-            x[i] = v/A[i][i]
+            for j in range(i + 1, n):
+                suma = suma + A[i][j] * x[j]
+            v = b[i] - suma
+            x[i] = v / A[i][i]
 
         return x
+
+
+def search_pivot(matrix, column):
+    max = 0
+    index = 0
+    for i in range(column, len(matrix[column])):
+        if max < abs(matrix[i][column]):
+            max = abs(matrix[i][column])
+            index = i
+    return index
+
+
+def interschimba_linii(A, l, index):
+    A[[l, index]] = A[[index, l]]
+    return A
+
+
+def gauss_algorithm(A, b, n):
+    l = 0
+    index = search_pivot(A, l)
+    A = interschimba_linii(A, l, index)
+    a_prim = np.zeros((n, n))
+    b_prim = np.zeros(n)
+    while l < n - 1 and A[l][l] != 0:
+        a_prim = A
+        for i in range(l + 1, n):
+            f = A[i][l] / A[l][l]
+            for j in range(l + 1, n):
+                a_prim[i][j] = A[i][j] - f * A[l][j]
+            b_prim[i] = b[i] - f * b[l]
+            a_prim[i][l] = 0
+        l += 1
+        index = search_pivot(a_prim, l)
+        A = interschimba_linii(a_prim, l, index)
+        print("A = ", A)
+
+    if A[l][l] == 0:
+        print("matrice singulara")
+    else:
+        x = substitution_method(A, b_prim, n)
+        diff = A @ x - b_prim
+        for o in diff:
+            if int(o) !=0:
+                print("Solutie gresita")
+            else:
+                return A,b_prim, 0
+
 
 
 if __name__ == '__main__':
     n = 5
     A = np.random.randint(low=1, high=100, size=n * n).reshape(n, n)
-    A = np.triu(A)
+    # A = np.triu(A)
     b = np.random.randint(low=1, high=100, size=n)
-    print("A= ",A)
-    print("b=",b)
-    x = substitution_method(A, b, n)
+    # print("A = ", A)
+    # print("b =", b)
+    # x = substitution_method(A, b, n)
+    #
+    # diff = A @ x - b
+    # for i , o in enumerate(diff):
+    #     diff[i] = int(o)
 
-    diff = A @ x - b
-    print("diff= ", diff)
+    # print("diff= ", diff)
+
+    gauss_algorithm(A, b, n)
