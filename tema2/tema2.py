2772be672372cef438c4a24727769fc63c5632fe
Alexandra <alexandramosoiu19@gmail.com>
2022-03-14 23:41:18 +0200

    almost done

--- tema2/tema2.py
+++ tema2/tema2.py
@@ -27,41 +27,42 @@ def search_pivot(matrix, column):
     return index
 
 
-def interschimba_linii(A, l, index):
+def interschimba_linii(A, b, l, index):
     A[[l, index]] = A[[index, l]]
-    return A
+    aux = b[index]
+    b[index] = b[l]
+    b[l]=aux
+    return A, b
 
 
 def gauss_algorithm(A, b, n):
     l = 0
     index = search_pivot(A, l)
-    A = interschimba_linii(A, l, index)
-    a_prim = np.zeros((n, n))
-    b_prim = np.zeros(n)
-    while l < n - 1 and A[l][l] != 0:
-        a_prim = A
+    A, b = interschimba_linii(A, b, l, index)
+    b_prim = b
+    a_prim = A
+    while l < n - 1 and a_prim[l][l] != 0:
         for i in range(l + 1, n):
-            f = A[i][l] / A[l][l]
+            f = a_prim[i][l] / a_prim[l][l]
             for j in range(l + 1, n):
-                a_prim[i][j] = A[i][j] - f * A[l][j]
-            b_prim[i] = b[i] - f * b[l]
+                a_prim[i][j] = a_prim[i][j] - f * a_prim[l][j]
+            b_prim[i] = b_prim[i] - f * b_prim[l]
             a_prim[i][l] = 0
         l += 1
         index = search_pivot(a_prim, l)
-        A = interschimba_linii(a_prim, l, index)
+        a_prim, b_prim = interschimba_linii(a_prim, b_prim, l, index)
         print("A = ", A)
 
     if A[l][l] == 0:
         print("matrice singulara")
     else:
-        x = substitution_method(A, b_prim, n)
-        diff = A @ x - b_prim
+        x = substitution_method(a_prim, b_prim, n)
+        diff = a_prim @ x - b_prim
         for o in diff:
-            if int(o) !=0:
+            if int(o) != 0:
                 print("Solutie gresita")
             else:
-                return A,b_prim, 0
-
+                return a_prim, b_prim, 0
 
 
 if __name__ == '__main__':
