28d455943089828d34b58057c4012b053bc840c1
Iordan Cosmina <58854421+Cosmina02@users.noreply.github.com>
2022-04-05 22:38:19 +0300

    functii ajutatoare

--- tema5/main.py
+++ tema5/main.py
@@ -1,16 +1,46 @@
-# This is a sample Python script.
+import copy
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import numpy as np
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def get_indexes(A):
+    maxim = 0
+    n = len(A)
+    p = q = 0
+    for i in range(2, n):
+        for j in range(1, i):
+            if abs(A[i][j]) > maxim:
+                maxim, p, q = abs(A[i][j]), i, j
+    return p, q
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def get_theta(A, p, q):
+    alfa = (A[p][p] - A[q][q]) / 2 * A[p][q]
+    if alfa >= 0:
+        t = -alfa + np.sqrt(alfa ** 2 + 1)
+    else:
+        t = -alfa + np.sqrt(alfa ** 2 + 1)
+    c = 1 / np.sqrt(1 + t ** 2)
+    s = t / np.sqrt(1 + t ** 2)
+    return t, c, s
+
+def rotate(n,p,q,c,s):
+    pass
+
+def check_matrix(A):
+    n = len(A)
+    for i in range(0, n):
+        for j in range(0, n):
+            if i != j and A[i][j] != 0:
+                return False
+    return True
+
+
+def jacobi_method(A):
+    n = len(A)
+    kmax = 10000
+    k = 0
+    U = np.identity(n)
+    p, q = get_indexes(A)
+    t, c, s = get_theta(A, p, q)
+    A_init = copy.deepcopy(A)
